78cacd82b09f6e5ca0dc961dd3ee105aabfe6feb
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-11-24 22:27:22 +0300

    crouch movement and animation

--- History Project/Assets/Scripts/PlayerMovement.cs
+++ History Project/Assets/Scripts/PlayerMovement.cs
@@ -4,35 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
+    [SerializeField] float playerSpeed = 5f;
     [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] float crouchSpeed = 3f;
+    [SerializeField] float upstairsSpeed = 3f;
     [SerializeField] float jumpTime = 0.3f;
     [SerializeField] Transform feetPos;
     [SerializeField] float feetRadius = 0.5f;
 
     Rigidbody2D rb;
     Animator animator;
+    SpriteRenderer sprite;
+    Transform body;
 
     float axisX;
     float axisY;
     float timeSinceJump = 0f;
+    float xSpeed;
+    float ySpeed;
 
     bool isJumping;
+    bool canJump=true;
     bool isGroundTouched;
+    bool isCrouched;
+    bool canGoUp=false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
+        body = transform.Find("BodyStructure");
+        sprite = GetComponent<SpriteRenderer>();
+        xSpeed = playerSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         axisX = Input.GetAxisRaw("Horizontal");
-        //axisY = Input.GetAxisRaw("Vertical");
+        axisY = Input.GetAxisRaw("Vertical");
+
+        FlipBody();
+
+        //if(axisY!=0 && canGoUp)
+        //{
+        //    ySpeed = upstairsSpeed;
+        //}
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 1f, LayerMask.GetMask("Stairs"));
+        if(hit)
+        {
+            ySpeed = upstairsSpeed;
+        }
 
         Jump();
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Crouch();
+        }
     }
 
     private void Jump()
@@ -41,8 +70,8 @@ public class PlayerMovement : MonoBehaviour
 
         if (isGroundTouched)
         {
-            axisY = 0f;
-            if (Input.GetKeyDown(KeyCode.Space))
+            jumpSpeed = 0;
+            if (Input.GetKeyDown(KeyCode.Space) && canJump)
             {
                 isJumping = true;
             }
@@ -50,11 +79,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) && isJumping)
         {
-            axisY = 1f;
+            ySpeed = jumpSpeed;
             timeSinceJump += Time.deltaTime;
             if (timeSinceJump > jumpTime)
             {
-                axisY = -1f;
+                ySpeed = -jumpSpeed;
                 isJumping = false;
                 timeSinceJump = 0f;
             }
@@ -69,9 +98,65 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(speed * axisX, jumpSpeed * axisY);
+        rb.velocity = new Vector2(xSpeed * axisX, ySpeed * axisY);
+        //Debug.Log(rb.velocity);
+        //Debug.Log("yspeed  "+ySpeed);
+        //Debug.Log("axisY  "+axisY);
 
         animator.SetFloat("SpeedX", axisX);
         animator.SetFloat("SpeedY", axisY);
     }
+
+    private void FlipBody()
+    {
+        if (axisX == -1)
+        {
+            body.localScale = new Vector3(-1, body.localScale.y, body.localScale.z);
+            sprite.flipX = true;
+        }
+        else if (axisX == 1)
+        {
+            body.localScale = new Vector3(1, body.localScale.y, body.localScale.z);
+            sprite.flipX = false;
+        }
+
+    }
+
+    private void Crouch()
+    {
+        if(!isCrouched)
+        {
+            sprite.enabled = false;
+            body.gameObject.SetActive(true);
+            canJump = false;
+            animator.SetBool("isCrouch", true);
+            xSpeed = crouchSpeed;
+            isCrouched = true;
+        }
+        else
+        {
+            sprite.enabled = true;
+            body.gameObject.SetActive(false);
+            canJump = true;
+            animator.SetBool("isCrouch", false);
+            xSpeed = playerSpeed;
+            isCrouched = false;
+        }
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if(collision.CompareTag("Stairs"))
+    //    {
+    //        canGoUp = true;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Stairs"))
+    //    {
+    //        canGoUp = false;
+    //    }
+    //}
 }
